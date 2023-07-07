1df5a400c5deebec14b5fd00117d97e292f98570
cyber_forge <cyberforge1@gmail.com>
2023-07-07 12:18:06 +1000

    first commit

---
+++ imageGeneration/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Prompt, Image
+
+
+def prompt(request):
+    '''Show all prompts'''
+    prompt = Prompt.objects.all()
+    context = {'prompt': prompt}
+    return render(request, 'imageGeneration/prompt.html', context)
+
+
+def image(request):
+    '''Show all images'''
+    image = Image.objects.all()
+    context = {
+        'image': image,
+    }
+    return render(request, 'imageGeneration/image.html', context)
